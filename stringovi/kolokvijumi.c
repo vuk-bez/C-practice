65ab044f60cf1dffa512cffc2bf1f5504e630f65
vukbez <bezmarevicvuk2@gmail.com>
2026-03-31 12:25:50 +0200

    Test

---
+++ stringovi/kolokvijumi.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ZAD 1. Napisati program koji provjerava da li je email adresa validna.
+// Email adresa je validna ako je formata personal_info@domain. 
+// Personal_info mora biti duzi od 3 karaktera, sadrzati samo brojeve, slova i ".-_"
+// Domain mora biti duzi od 3 karaktera, samo slova i mora imati bar jednu ".", ne smije pocinjati njome
+
+void zad1(char* adresa) {
+    int prijeAt = 0;
+    int posleAt = 0;
+    int AtPosition = 0;
+    int indicatorAt = 0;
+    int tacka = 0;
+
+    for(int i = 0; adresa[i] != '\0'; i++) {
+        
+        if(adresa[i] == '@') {
+            indicatorAt ++;
+            AtPosition = i;
+            continue;
+        }
+        if(AtPosition != 0 && adresa[AtPosition + 1] == '.') {
+            printf("Nije validna!");
+            return;
+        } 
+        if(((adresa[i] >= 'A' && adresa[i] <= 'Z') || (adresa[i] >= 'a' && adresa[i] <= 'z') || adresa[i] == '.' || adresa[i] == '-' || adresa[i] == '_') && !indicatorAt) {
+            prijeAt++;
+        } else if (((adresa[i] >= 'A' && adresa[i] <= 'Z') || (adresa[i] >= 'a' && adresa[i] <= 'z') || adresa[i] == '.') && indicatorAt) {
+            posleAt++;
+            if(adresa[i] == '.') tacka++;
+        } else {
+            printf("Nije validna!");
+            return;
+        }
+        
+    }
+
+    if(posleAt > 3 && prijeAt > 3 && tacka > 0) printf("Adresa je validna!");
+    else printf("Nije validna!");
+    
+}
+
+
+int main() {
+    
+    char* adresa = (char*)malloc(100*sizeof(char));
+    scanf("%s", adresa);
+    zad1(adresa);
+
+    return 0;
+}
